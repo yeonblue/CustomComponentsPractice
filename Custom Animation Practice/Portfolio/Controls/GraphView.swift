542bd19360c36a7daefa01b346cd5ab5914f4552
최서연 <yeondublue@naver.com>
2022-10-03 21:14:36 +0900

    add charts

--- Custom Animation Practice/Portfolio/Controls/GraphView.swift
+++ Custom Animation Practice/Portfolio/Controls/GraphView.swift
@@ -49,17 +49,76 @@ class GraphView: UIView {
   lazy var columnWidth: CGFloat = {
     return bounds.width / CGFloat(days.count)
   }()
+  
+  lazy var data: [CGPoint] = {
+    var array = [CGPoint]()
+    
+    for (idx, day) in days.enumerated() {
+      let point = CGPoint(x: columnWidth * CGFloat(idx), y: day / eachLabel * gap)
+      array.append(point)
+    }
+    
+    return array
+  }()
 
   override func draw(_ rect: CGRect) {
+    let context = UIGraphicsGetCurrentContext()!
+    drawText(context: context)
     
+    
+    context.translateBy(x: columnWidth/2 + 10, y: 0)
+    context.scaleBy(x: 1, y: -1)
+    context.translateBy(x: 0, y: -bounds.height)
+    context.translateBy(x: 0, y: gap)
+
+    // clip
+    context.saveGState()
+    context.addLines(between: data)
+    context.strokePath()
+    context.closePath()
+    context.clip()
+    
+    drawGradient(context: context)
+    context.restoreGState()
+    
+    context.addLines(between: data)
+    context.strokePath()
+    context.restoreGState()
   }
   
   func drawGradient(context: CGContext) {
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    let colors: NSArray = [UIColor.blue.withAlphaComponent(0.3).cgColor,
+                           UIColor.systemBlue.withAlphaComponent(0.8).cgColor]
+    
+    let locations: [CGFloat] = [0.0, 1.0]
+    let gradient = CGGradient(colorsSpace: colorSpace, colors: colors, locations: locations)
+    
+    context.drawLinearGradient(gradient!, start: .zero, end: CGPoint(x: 0, y: bounds.height), options: [])
+    
+    // context.drawRadialGradient도 존재
   }
   
   func drawText(context: CGContext) {
+    let font = UIFont(name: "Avenir-Light", size: 12)!
+    let attributes = [NSAttributedString.Key.font: font]
+    let maxValue = CGFloat(days.max()!)
+    context.saveGState()
+    for i in 0..<5 {
+      context.translateBy(x: 0, y: gap)
+      
+      context.setStrokeColor(UIColor.darkGray.cgColor)
+      context.setLineWidth(1)
+      context.addLines(between: [CGPoint(x: 35, y: 0), CGPoint(x: bounds.width, y: 0)])
+      context.strokePath()
+      
+      let text = "\(maxValue - eachLabel * CGFloat(i))" as NSString
+      let size = text.size(withAttributes: attributes)
+      text.draw(at: CGPoint(x: 6, y: -size.height/2), withAttributes: attributes)
+    }
+    
+    context.restoreGState()
   }
-  
 }
 
 
